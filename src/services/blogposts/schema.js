64202e7cce52abe6182d38987feaeb9681b11837
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-10-06 18:15:35 +0200

    timestamps added

--- src/services/blogposts/schema.js
+++ src/services/blogposts/schema.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const commentSchema = new Schema({
-  text: { type: String, required: true },
-  user_name: { type: String, required: true }
-});
+const commentSchema = new Schema(
+  {
+    text: { type: String, required: true },
+    user_name: { type: String, required: true }
+  },
+  {
+    timestamps: true
+  }
+);
 
 const blogPostSchema = new Schema(
   {
